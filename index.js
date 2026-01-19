2d8869596a4e14fb98e65fd329e1927a5920ae29
MengSonly17 <msonly114@gmail.com>
2026-01-19 23:20:02 +0700

    v2.0 fixed a route.

--- index.js
+++ index.js
@@ -14,48 +14,51 @@ const products = [
 
 // get all products
 app.get('/api/v1/products', (req, res) => {
+    const { name } = req.query;
+    let message = '';
+    let data = [];
+
+    if (name) {
+        const findProduct = products.filter(item => item.name.toLowerCase() == name);
+        data = [...findProduct];
+        message = 'Product was found.';
+    }
+    else {
+        message = 'All products.';
+        data = [...products];
+    }
+
     res.json({
-        'messsage' : 'All products.',
-        'data' : products,
-        'status' : '200'
+        'messsage': message,
+        'data': data,
+        'status': '200'
     });
+
 });
 
 // get products by id
-app.get('/api/v1/products/:id',(req,res)=>{
+app.get('/api/v1/products/:id', (req, res) => {
     const id = req.params.id;
-    const findProduct = products.find( item => item.id == id );
+    const findProduct = products.find(item => item.id == id);
 
     res.json({
-        'messsage' : 'Find.',
-        'data' : findProduct,
-        'status' : '200'
+        'messsage': 'Find.',
+        'data': findProduct,
+        'status': '200'
     });
 });
 
-// get products by name 
-app.get('/api/v1/product',(req,res)=>{
-    const {name} = req.query; 
-
-    const findProduct = products.filter(item => item.name.toLowerCase() == name);
-
-    res.json({
-        'messsage' : 'Find by name.',
-        'data' : findProduct,
-        'status' : '200'
-    });
-});
 
 // create product
-app.post('/api/v1/products',(req,res)=>{
-    const {name,price,qty} = req.body;
-    let id = (products.length >= 1) ? (products[products.length-1].id  + 1) : 1;
+app.post('/api/v1/products', (req, res) => {
+    const { name, price, qty } = req.body;
+    let id = (products.length >= 1) ? (products[products.length - 1].id + 1) : 1;
 
     let prod = {
-        "id" : id,
-        "name" : name,
-        "price" : price,
-        "qty" : qty
+        "id": id,
+        "name": name,
+        "price": price,
+        "qty": qty
     }
 
     products.push(prod);
@@ -63,19 +66,19 @@ app.post('/api/v1/products',(req,res)=>{
     console.log(products)
 
     res.json({
-        'message' : 'Create Product was successfully.',
-        'data' : products,
-        'status' : '201'
+        'message': 'Create Product was successfully.',
+        'data': products,
+        'status': '201'
     })
 });
 
 // update product
-app.put('/api/v1/products/:id',(req,res)=>{
+app.put('/api/v1/products/:id', (req, res) => {
     const id = Number.parseInt(req.params.id);
-    const {name,price,qty} = req.body;
+    const { name, price, qty } = req.body;
 
-    products.forEach( item => {
-        if(item.id == id){
+    products.forEach(item => {
+        if (item.id == id) {
             item.name = name;
             item.price = price;
             item.qty = qty;
@@ -84,22 +87,22 @@ app.put('/api/v1/products/:id',(req,res)=>{
     });
 
     res.json({
-        'message' : 'Product was deleted.',
-        'status' : '200',
-        'data' : products
+        'message': 'Product was deleted.',
+        'status': '200',
+        'data': products
     });
 });
 
 // delete 
-app.delete('/api/v1/products/:id',(req,res)=>{
+app.delete('/api/v1/products/:id', (req, res) => {
     const id = Number.parseInt(req.params.id);
     let message = '';
     let status = '';
 
     const findIdx = products.findIndex(item => item.id == id);
 
-    if(findIdx != -1){
-        products.splice(findIdx,1)
+    if (findIdx != -1) {
+        products.splice(findIdx, 1)
 
         message = 'Delete Product was successfully.';
         status = '200';
@@ -108,11 +111,11 @@ app.delete('/api/v1/products/:id',(req,res)=>{
         message = 'Invalid id. Please try again.';
         status = '204';
     }
-    
+
 
     res.json({
-        'message' : message,
-        'status' : status
+        'message': message,
+        'status': status
     });
 });
 
